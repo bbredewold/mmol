47dc75d421fce83c530dbf88bf1c226ee024b915
Bert Bredewold <bbredewold@gmail.com>
2023-07-28 23:01:00 +0200

    Cleanup AppController

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,4 @@
 <?php
 
-use App\Http\Controllers\AppController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [AppController::class, 'index']);
-Route::get('/setup', [AppController::class, 'setup']);
-Route::get('/settings', [AppController::class, 'settings']);
